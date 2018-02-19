91c153ef6ae0a0e141964868f0e02104fd9da1d6
Beth Punzalan <bethelizganda22@gmail.com>
2018-02-20 01:54:46 +0800

    latest commit for Final Defense

--- SMS/send_sms.php
+++ SMS/send_sms.php
@@ -3,7 +3,7 @@
 if (!empty($_POST['message'])) {
   $phone = $_POST['phone'];
   $msg = implode("\n\n", $_POST['message'])."\n\n-----";
-  $result = itexmo($phone,$msg,"TR-ELIZA306457_AUYQD");
+  $result = itexmo($phone,$msg,"TR-SHAIR374833_PHL2Z");
   if ($result == ""){
     echo "iTexMo: No response from server!!!";  
   } else if ($result == 0){
